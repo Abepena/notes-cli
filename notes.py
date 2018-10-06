596456afe6948566245df101326d8d3da440b645
Abraham Pena <pena.abe@gmail.com>
2018-10-05 23:50:11 -0400

    added search function and delete functoin

--- notes.py
+++ notes.py
@@ -21,7 +21,7 @@ def menu_loop():
     choice = None
     #Print out the menu until user types 'q'
     while choice != 'q':
-        print("Enter 'q' to quit")
+        print("\nEnter 'q' to quit")
         for key, val in menu.items():
             #key is choice letter, value is the function assciated w/ that letter
             print(f'{key}) {val.__doc__}') 
@@ -42,29 +42,42 @@ def add_entry():
             Entry.create(content=data)
             print('Saved!')
 
-def view_entries():
+def view_entries(search_query=None):
     """View all entries"""
     entries = Entry.select().order_by(Entry.timestamp.desc())
+    
+    if search_query:
+        """Same as SQL query:
+            SELECT * FROM entries
+            WHERE content LIKE '%search_query%'
+            ORDER BY timestamp DESC
+        """
+        entries = entries.where(Entry.content.contains(search_query))
     
     for entry in entries:
         timestamp = entry.timestamp.strftime('%A %B %d, %Y:%M%p')
         print(timestamp)
-        print('=' * len(timestamp))
-        print(entry.content)
-        print('n) next entry')
-        print('q) return to main menu')
+        print('=' * len(timestamp) + '\n')
+        print(entry.content + '\n')
+        print('n) Next entry')
+        print('q) Return to main menu')
+        print('d) Delete this entry')
 
         next_action = input('Action: [N/q]').lower().strip()
 
         if next_action == 'q':
             break
+        elif next_action == 'd':
+            delete_entry(entry)
 
 def search_entries():
-    pass
+    """Search through your note entries"""
+    view_entries(input('Search Query: '))
 
 def delete_entry(entry):
     """Delete an entry"""
-    pass
+    if input('\nAre you sure? [Y/n]').lower() == 'y':
+        entry.delete_instance()
 
 def initialize():
     """ Create db and tables if they don't exist """
